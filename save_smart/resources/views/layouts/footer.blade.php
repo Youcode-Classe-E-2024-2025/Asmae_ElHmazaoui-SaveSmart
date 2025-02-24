de8fe09362dc66b31919a83c009041ff9e0ae46e
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-24 21:54:09 +0000

    implémentation du footer dans un fichier séparé

---
+++ save_smart/resources/views/layouts/footer.blade.php
@@ -0,0 +1,27 @@
+ <!-- Footer -->
+ <footer class="bg-gray-800 text-white py-8 fade-in">
+        <div class="container m-4 ">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div>
+                    <h4 class="text-lg text-yellow-500  font-bold">À propos</h4>
+                    <p class="mt-2 text-sm">Nous sommes une entreprise dédiée à fournir des services de qualité.</p>
+                </div>
+                <div>
+                    <h4 class="text-lg text-yellow-500  font-bold">Liens rapides</h4>
+                    <ul class="mt-2 space-y-2">
+                        <li><a href="#" class="hover:underline">Accueil</a></li>
+                        <li><a href="#" class="hover:underline">Services</a></li>
+                        <li><a href="#" class="hover:underline">Contact</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="text-lg text-yellow-500  font-bold">Suivez-nous</h4>
+                    <div class="mt-2 space-x-4">
+                        <a href="#" class="hover:text-gray-300">Facebook</a>
+                        <a href="#" class="hover:text-gray-300">Twitter</a>
+                        <a href="#" class="hover:text-gray-300">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
