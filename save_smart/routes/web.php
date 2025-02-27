0535ca584d60536c679f2bec50850ea612f8ec20
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-27 12:19:18 +0000

    ajout des routes pour la gestion des ctégories

--- save_smart/routes/web.php
+++ save_smart/routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TransactionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Dashboard', [DashboardController::class, 'showDashboard'])->name('Dashboard');
+
 
 // Auth routes
 Route::get('/signup', [AuthController::class, 'showSignUp']);
@@ -15,3 +19,15 @@ Route::get('/login', [AuthController::class, 'showLogin']);
 Route::post('/signup', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout']);
+
+// category routes
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+
+// transactions routes
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+//     Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+//     Route::put('/transactions/{id}', [TransactionController::class, 'update'])->name('transactions.update');
+//     Route::delete('/transactions/{id}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
+// });
