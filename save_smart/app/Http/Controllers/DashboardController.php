a1d05c5c1b1929f83021cadc0cd8a83b4bf3e2ff
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-28 15:24:02 +0000

    instancier un objet depuis la classe transaction pour appler la methode index dans le controlleur de transaction

--- save_smart/app/Http/Controllers/DashboardController.php
+++ save_smart/app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Category;
+use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
@@ -10,9 +11,10 @@ class DashboardController extends Controller
 {
     // function pour appeler la vue dashboard 
      public function showDashboard(){
-     
+       
+      $transactions = Transaction::where('user_id', Auth::id())->orderBy('date', 'desc')->get();
       $categories = Category::where('user_id', Auth::id())->get(); //Récupérer uniquement les catégories créées par l'utilisateur actuel.
-      return view('dashboard', compact('categories'));
+      return view('dashboard',  compact('transactions', 'categories'));
 
 
      }
