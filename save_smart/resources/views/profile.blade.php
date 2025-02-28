d3cfc6a2bdd68ed0594fb6b820468b9ac4bfb9dc
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-28 15:12:45 +0000

    modal pour ajouter un profile

--- save_smart/resources/views/profile.blade.php
+++ save_smart/resources/views/profile.blade.php
@@ -58,7 +58,30 @@
             </div>
         </div>
 
-       
+        <!-- Modal -->
+        <div id="profileModal" class="fixed top-0 left-0 w-full h-full flex items-center justify-center hidden modal-backdrop">
+            <div class="bg-gray-800 rounded shadow-lg p-8 w-96">
+                <h2 class="text-white text-xl mb-4">Add New Profile</h2>
+                <form id="profileForm">
+                    <div class="mb-4">
+                        <label for="name" class="block text-gray-300 text-sm font-bold mb-2">Name:</label>
+                        <input type="text" id="name" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-700 text-white" placeholder="Enter profile name">
+                    </div>
+
+                     <div class="mb-4">
+                        <label for="avatar" class="block text-gray-300 text-sm font-bold mb-2">Avatar:</label>
+                        <input type="file" id="avatar" name="avatar" accept="image/*" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-700 text-white" />
+                        <img id="avatarPreview" src="#" alt="Avatar Preview" class="hidden">
+                    </div>
+
+
+                    <div class="flex justify-end">
+                        <button type="button" id="cancelBtn" class="bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2">Cancel</button>
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Create</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <script>
