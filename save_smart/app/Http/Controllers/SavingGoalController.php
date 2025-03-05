8c54532ddf6701d488b514c6877a75ddb8a4685f
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-05 12:08:44 +0000

    implémentation des fonctionnalités pour les objectifs d epargne

--- save_smart/app/Http/Controllers/SavingGoalController.php
+++ save_smart/app/Http/Controllers/SavingGoalController.php
@@ -36,6 +36,34 @@ class SavingGoalController extends Controller
         return redirect()->route('Dashboard')->with('success', 'Objectif d\'épargne ajouté avec succès.');
     }
 
-   
+    // Methode pour la modification des objectifs d'épargne
+    public function update(Request $request, $id)
+    {
+        $goal = SavingGoal::where('id', $id)->where('user_id', Auth::id())->firstOrFail(); // Vérifier que l'objectif appartient à l'utilisateur
 
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'goal_amount' => 'required|numeric|min:0',
+            'deadline' => 'required|date|after_or_equal:today',
+        ]);
+
+        // Mettre à jour l'objectif
+        $goal->update([
+            'name' => $request->name,
+            'goal_amount' => $request->goal_amount,
+            'deadline' => $request->deadline,
+        ]);
+
+        return redirect()->route('Dashboard')->with('success', 'Objectif d\'épargne modifié avec succès.');
+    }
+
+    // Methode pour la suppression des objectifs d'épargne
+    public function destroy($id)
+    {
+        $goal = SavingGoal::where('id', $id)->where('user_id', Auth::id())->firstOrFail(); // Vérifier que l'objectif appartient à l'utilisateur
+        $goal->delete(); // Supprimer l'objectif
+
+        return redirect()->route('Dashboard')->with('success', 'Objectif d\'épargne supprimé avec succès.');
+    }
+    
 }
