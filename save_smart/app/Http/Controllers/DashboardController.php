52dda1e850eac7d1de38deed1231535709d51335
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-05 12:04:08 +0000

    récupérer les objectifs depuis la base de données pour les passer à la page dashboard

--- save_smart/app/Http/Controllers/DashboardController.php
+++ save_smart/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Transaction;
+use App\Models\SavingGoal;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
@@ -11,10 +12,11 @@ class DashboardController extends Controller
 {
     // function pour appeler la vue dashboard 
      public function showDashboard(){
-       
+    
+      $goals = SavingGoal::where('user_id', Auth::id())->orderBy('deadline', 'asc')->get(); // Récupérer les objectifs pour l'utilisateur connecté
       $transactions = Transaction::where('user_id', Auth::id())->orderBy('date', 'desc')->get();
       $categories = Category::where('user_id', Auth::id())->get(); //Récupérer uniquement les catégories créées par l'utilisateur actuel.
-      return view('dashboard',  compact('transactions', 'categories'));
+      return view('dashboard',  compact('goals','transactions', 'categories'));
 
 
      }
