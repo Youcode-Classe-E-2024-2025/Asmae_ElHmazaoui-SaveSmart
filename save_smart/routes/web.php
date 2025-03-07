b811efdfd3f2edf3b8f5888cf22e27c7a2c88f78
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-07 22:53:55 +0000

    modification de route de la deconnexion

--- save_smart/routes/web.php
+++ save_smart/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/register', [AuthController::class, 'showSignUp'])->name('register')
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // category routes
 Route::post('/categories', [CategoryController::class, 'store']);
